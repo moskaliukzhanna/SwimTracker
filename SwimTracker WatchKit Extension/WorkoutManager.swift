88c61a1192523fd0443e5ebb704f2399f60ae3f6
Zhanna Moskaliuk <octoberrock11@gmail.com>
2022-08-23 17:51:48 +0300

    Fixed the request for activity summies

--- SwimTracker WatchKit Extension/WorkoutManager.swift
+++ SwimTracker WatchKit Extension/WorkoutManager.swift
@@ -48,12 +48,18 @@ final class WorkoutManager: WorkoutManagerProtocol {
         print("Health data available \(HKHealthStore.isHealthDataAvailable() )")
         
         if HKHealthStore.isHealthDataAvailable() {
-            let allDataTypes = Set([
-                HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
+            let typesToRead = Set([
+                HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                HKObjectType.activitySummaryType()
             ])
             
-            self.hkHealthStore.requestAuthorization(toShare: allDataTypes,
-                                                    read: allDataTypes) { [weak self] sucess, error in
+            let typesToWrite = Set([
+                HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+              
+            ])
+            
+            self.hkHealthStore.requestAuthorization(toShare: typesToWrite,
+                                                    read: typesToRead) { [weak self] sucess, error in
                 guard let self = self else { return }
                 print("IS Succesful \(sucess)")
                 if sucess {
@@ -85,50 +91,41 @@ final class WorkoutManager: WorkoutManagerProtocol {
         let endDate = Date()
         
         let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictEndDate)
-//        let query = HKActivitySummaryQuery(predicate: predicate) { (query, summaries, error) in
-        let query = HKStatisticsQuery(quantityType: HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!, quantitySamplePredicate: predicate) { query, summary, error in
-            if let error = error {
-                print("Error - \(error)")
-            }
-            if let sample = summary {
-                let energy = sample.sumQuantity()?.doubleValue(for: HKUnit.kilocalorie()) ?? 0.0
-                print("Energy - \(energy)")
-                
-            }
-        }
+        let query = HKActivitySummaryQuery(predicate: predicate) { (query, summaries, error) in
+        
             print("2")
             
-//            guard let summaries = summaries, summaries.count > 0, let summary = summaries.first
-//            else {
-//                // No data returned. Perhaps check for error
-//                print("3")
-//                if let error = error {
-//                    print("Error - \(error)")
-//                }
-//                return
-//            }
-//
-//            let energyUnit   = HKUnit.kilocalorie()
-//            //            let standUnit    = HKUnit.count()
-//            //            let exerciseUnit = HKUnit.second()
-//
-//            let energy   = summary.activeEnergyBurned.doubleValue(for: energyUnit)
-//            //            let stand    = summary.appleStandHours.doubleValue(for: standUnit)
-//            //            let exercise = summary.appleExerciseTime.doubleValue(for: exerciseUnit)
-//            //
-//            let energyGoal   = summary.activeEnergyBurnedGoal.doubleValue(for: energyUnit)
-//            //            let standGoal    = summary.appleStandHoursGoal.doubleValue(for: standUnit)
-//            //            let exerciseGoal = summary.appleExerciseTimeGoal.doubleValue(for: exerciseUnit)
-//
-//            let energyProgress   = energyGoal == 0 ? 0 : energy / energyGoal
-//            //            let standProgress    = standGoal == 0 ? 0 : stand / standGoal
-//            //            let exerciseProgress = exerciseGoal == 0 ? 0 : exercise / exerciseGoal
-//            print("4")
-//            print("\(energyGoal)")
-//
-//        }
+            guard let summaries = summaries, summaries.count > 0, let summary = summaries.first
+            else {
+                // No data returned. Perhaps check for error
+                print("3")
+                if let error = error {
+                    print("Error - \(error)")
+                }
+                return
+            }
+
+            let energyUnit   = HKUnit.kilocalorie()
+            //            let standUnit    = HKUnit.count()
+            //            let exerciseUnit = HKUnit.second()
+
+            let energy   = summary.activeEnergyBurned.doubleValue(for: energyUnit)
+            //            let stand    = summary.appleStandHours.doubleValue(for: standUnit)
+            //            let exercise = summary.appleExerciseTime.doubleValue(for: exerciseUnit)
+            //
+            let energyGoal   = summary.activeEnergyBurnedGoal.doubleValue(for: energyUnit)
+            //            let standGoal    = summary.appleStandHoursGoal.doubleValue(for: standUnit)
+            //            let exerciseGoal = summary.appleExerciseTimeGoal.doubleValue(for: exerciseUnit)
+
+            let energyProgress   = energyGoal == 0 ? 0 : energy / energyGoal
+            //            let standProgress    = standGoal == 0 ? 0 : stand / standGoal
+            //            let exerciseProgress = exerciseGoal == 0 ? 0 : exercise / exerciseGoal
+            print("4")
+            print("\(energyGoal)")
+
+        }
         hkHealthStore.execute(query)
-//        print("1")
+        print("1")
         
         
     }
